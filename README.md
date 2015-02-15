eecb4ef2a4f706dde84e04fb14103ee4b1103b06
Sergio <sergioforerop@hotmail.com>
2015-02-15 15:29:33 -0500

    version 1

---
+++ README.md
@@ -0,0 +1,19 @@
+# RegresionLineal
+# para bajar el proyecto ejecutar el siguiente comando  $git clone https://github.com/sforerop/regresionpsp1
+
+# La URL en Heroku es : http://regresionpsp1.herokuapp.com/
+# puede probar con los siguientes valores 130,186->650,699->99,132->150,272->128,291->302,331->95,199->945,1890->368,788->961,1601
+
+# en la ruta psp3\src\site\resource\PSPtrabajo3.docx se encuentra la documentacion de PSP1.
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/edu/uniandes/ecos/Main.java
@@ -0,0 +1,76 @@
+package edu.uniandes.ecos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main extends HttpServlet {
+
+    public static String texto = "";
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()), "/*");
+        server.start();
+        server.join();
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter pagina=resp.getWriter();
+        resp.setContentType("text/html");
+        String valor = req.getParameter("elementos");        
+        App app = new App();        
+        App.cargarDatosPantalla(valor);
+        PrintWriter out = resp.getWriter();
+        out.print("B0 =" + App.getB0() + "\n");
+        out.print("B1 =" + App.getB1()+ "\n");
+        out.print("R =" + App.getR()+ "\n");
+        out.print("R2 =" + App.getR2()+ "\n");
+        out.print("YK =" + App.getYK()+ "\n");
+        out.print("XK =" + App.getXK()+ "\n");
+        
+        
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        if(req.getParameter("elementos") != null){
+            showHome(req, resp);
+        }else{
+        PrintWriter out = resp.getWriter();
+        out.print("<html>\n"
+                + "<body>\n"
+                + "<form action=\"/Main\" method=\"POST\">\n"
+                +"Ingrese loas datos X,Y separados por comas y separe las duplas con -> \nEjemplo: 0,1->2,5->3,6\n"
+                + "<br/>\n"
+                + "Datos: <input type=\"text\" name=\"elementos\">\n"
+                + "<br />\n"
+                + "<input type=\"submit\" value=\"Submit\" />\n"
+                + "</form>\n"
+                + "</body>\n"
+                + "</html>");
+        }        
+    }
+
+    private void showHome(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        
+        
+    }
+}
